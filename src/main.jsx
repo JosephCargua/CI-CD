28cb7d02531680f50bdbde42d52d1661b632106b
JosephCargua <josepcargua94@gmail.com>
2024-12-23 22:07:09 -0500

    Creacion del detalle de la compra

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import MetodoPago from './MetodoPago.jsx'
 
 /*Leer la etiqueta root del <div id="root"></div> 
 y monta la aplicacion de react en ese div*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   //Archivo principal de la app de react
   <React.StrictMode>
-    {/*Aplicacion montada*/}
     <App />
   </React.StrictMode>,
 )
